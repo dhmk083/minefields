c7acd4477214784760da3f2ea8d2f36f7aa2c347
dhmk083 <dhmk083@gmail.com>
2020-04-02 13:09:33 +0300

    Updates

---
+++ precache-manifest.9dc44daf114e252068977e410b131091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb9ebf00ce7f46eee6deb46251aa741",
+    "url": "/minefields/index.html"
+  },
+  {
+    "revision": "f25d024e08541f6a8abe",
+    "url": "/minefields/static/css/main.80eef7ef.chunk.css"
+  },
+  {
+    "revision": "6b29a67fb6290d261c35",
+    "url": "/minefields/static/js/2.94e7c745.chunk.js"
+  },
+  {
+    "revision": "57f5c77c6cb93e0937628f0205081484",
+    "url": "/minefields/static/js/2.94e7c745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25d024e08541f6a8abe",
+    "url": "/minefields/static/js/main.d6e51338.chunk.js"
+  },
+  {
+    "revision": "8766ef709fc7efb41727",
+    "url": "/minefields/static/js/runtime-main.b5a67520.js"
+  }
+]);
